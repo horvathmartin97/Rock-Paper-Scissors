bc82708d0fa3079a558bbb6c39e412daf652f3bd
Eric Kariuki <rico@Erics-MacBook-Air.local>
2026-04-15 19:39:50 +0300

    implement game function logic

--- main.js
+++ main.js
@@ -8,24 +8,60 @@ function playRound(playerSelection, computerSelection) {
   const player = playerSelection.toLowerCase();
   const computer = computerSelection.toLowerCase();
 
-  if (player === computer) {
-    return `It's a tie! Both chose ${player}`;
-  }
 
-  if (player === "rock" && computer === "scissors") {
-    return "You Win! Rock beats Scissors";
-  }
+  switch(true){
+    case (player === computer):
+      return "tie"
 
-  if (player === "paper" && computer === "rock") {
-    return "You Win! Paper beats Rock";
-  }
+    case (player === "rock" && computer === "scissors"):
+      return [true, "You win!"]
 
-  if (player === "scissors" && computer === "paper") {
-    return "You Win! Scissors beats Paper";
-  }
+    case (player === "paper" && computer === "rock"):
+      return [true, "You win!"]
 
-  return `You Lose! ${computer.charAt(0).toUpperCase() + computer.slice(1)} beats ${player.charAt(0).toUpperCase() + player.slice(1)}`;
+    case (player === "scissors" && computer === "paper"):
+      return [true, "You win!"]
+
+    default:
+      return [false, "You lose"]
+  }
 }
-const playerSelection = "paper";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+
+function game(){
+  let playerScore = 0
+  let computerScore = 0
+  const playerChoices = ["rock", "paper", "scissors"]
+  
+  for(let i = 1; i <= 5; i++){
+    let playerInput = prompt("Pick one weapon(Rock, Paper or Scissors): ").toLowerCase()
+  
+    while(true){
+      if(playerChoices.includes(playerInput)){
+        break; 
+      }
+  
+      playerInput = prompt("Wrong input. Pick a valid weapon(Rock, Paper or Scissors): ").toLowerCase()
+    }
+
+    const computerSelection = computerPlay()
+    let gameResult = playRound(playerInput, computerSelection) //* [true, "You win!"] || [false, "You lose!"] || "tie"
+
+    if(gameResult[0]){
+      playerScore += 1
+    }else if(gameResult === "tie"){
+      continue
+    }else{
+      computerScore += 1
+    }
+
+  }
+
+  if(playerScore === computerScore){
+      return "It was a Tie!"
+    }else if(playerScore > computerScore){
+      return `You win! You won ${playerScore} games`
+    }else{
+      return `Computer wins! Computer won ${computerScore} games`
+    }
+}
+console.log(game())
